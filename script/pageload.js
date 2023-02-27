7de8ca4a5d20cdeb2a3b88b7c823a81c173ee0ac
Kevin Lee Dioso Cardoza <49277166+Kiboot@users.noreply.github.com>
2023-02-28 01:33:48 +0800

    Add files via upload

--- script/pageload.js
+++ script/pageload.js
@@ -2,4 +2,11 @@ async function addNavbar() {
     const resp = await fetch("navbar.html");
     const html = await resp.text();
     document.body.insertAdjacentHTML("afterbegin", html);
+
+    const a = Math.random() * (20-1) + 1;
+    if(parseInt(a) == 1){
+        document.body.setAttribute("style",'background-image:url("img/obunga.png")');
+    }
+    
+
 }
